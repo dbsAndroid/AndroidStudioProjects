dce18d5505b5b40d2aa769bbe6d4ae7a4a00dfe7
dbsAndroid <10353856@mydbs.ie>
2017-09-11 10:15:25 +0100

    sandbox scroll

---
+++ SandboxLocalisationLanguage/app/src/main/java/com/example/ferrari7/sandboxlocalisationlanguage/HelloAndroid.java
@@ -0,0 +1,18 @@
+package com.example.ferrari7.sandboxlocalisationlanguage;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HelloAndroid extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+
+        // Required call through to Activity.onCreate()
+        // Restore any saved instance state
+        super.onCreate(savedInstanceState);
+
+        // Set up the application's user interface (content view)
+        setContentView(R.layout.helloandroid);
+
+
+    }
+}
